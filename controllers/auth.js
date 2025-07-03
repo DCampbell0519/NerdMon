1f8c4a3c49db9df531800c780f75b5e0bfd891f2
Donovan Campbell <donovan@dlcentertainment.net>
2025-07-03 16:36:51 -0700

    sign up page debugged

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,6 +10,19 @@ router.get('/sign-up', (req, res) => {
     res.render('auth/sign-up.ejs')
 });
 
+router.post('/sign-up', async (req, res) => {
+    res.send('Got your info' + req.body.username)
+    const userInDatabase = await User.findOne({ username: req.body.username })
+
+    if (userInDatabase) {
+        return res.send('User already exists')
+    }
+})
+
 router.get('/sign-in', (req, res) => {
     res.render('auth/sign-in.ejs');
 });
+
+
+
+module.exports = router;
