2db5a43854995f862c05911d83ed0600143e3b93
Donovan Campbell <donovan@dlcentertainment.net>
2025-07-03 17:40:20 -0700

    auth routes tested and verified

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,18 +11,47 @@ router.get('/sign-up', (req, res) => {
 });
 
 router.post('/sign-up', async (req, res) => {
-    res.send('Got your info' + req.body.username)
     const userInDatabase = await User.findOne({ username: req.body.username })
 
     if (userInDatabase) {
         return res.send('User already exists')
     }
+    
+    if (req.body.password !== req.body.confirmPassword) {
+        return res.send('Passwords do not match')
+    }
+
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+    req.body.password = hashedPassword;
+
+    const newUser = await User.create(req.body)
+    res.redirect('/')
 })
 
 router.get('/sign-in', (req, res) => {
     res.render('auth/sign-in.ejs');
 });
 
+router.post('/sign-in', async (req, res) => {
+    const userInDatabase = await User.findOne({ username: req.body.username })
+    if (!userInDatabase) {
+        return res.send('User does not exist')
+    } 
+
+    const validPassword = bcrypt.compareSync(
+        req.body.password,
+        userInDatabase.password,
+    )
+    if (!validPassword) {
+        res.send('Login failed.  Please try again.')
+    }
+
+    req.session.user = {
+        username: userInDatabase.username,
+        _id: userInDatabase._id,
+    }
+    res.redirect('/')
+})
 
 
 module.exports = router;
